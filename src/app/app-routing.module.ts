96e4cadfd6c6cfb08f46abe6633898a1789588f5
KeviinSerna <kevinsernap@gmail.com>
2023-08-31 01:03:26 -0500

    add routing and change in module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { AboutComponent } from './modules/about/about.component';
+import { ContactComponent } from './modules/contact/contact.component';
+import { ProjectsComponent } from './modules/projects/projects.component';
+import { SharedComponent } from './modules/shared/shared.component';
+import { SkillsComponent } from './modules/skills/skills.component';
 
 const routes: Routes = [];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+import { ProjectsComponent } from './modules/projects/projects.component';
+import { SkillsComponent } from './modules/skills/skills.component';
+import { AboutComponent } from './modules/about/about.component';
+import { ContactComponent } from './modules/contact/contact.component';
+import { SharedComponent } from './modules/shared/shared.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    AboutComponent,
+    ContactComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
